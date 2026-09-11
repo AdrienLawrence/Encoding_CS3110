358fab93df47c0e7c6acc578b57d53a2655d60d1
Adrien Lawrence <firestar9jr@gmail.com>
2026-09-11 15:56:38 -0400

    Project Skeleton Created
    To the best of my ability, i have attempted to replicate the starter
    project structure of a real starter project

---
+++ LawrenceEncoding/src/edu/westga/cs3110/encoding/model/Codepoint.java
@@ -0,0 +1,27 @@
+package edu.westga.cs3110.encoding.model;
+
+public class Codepoint {
+	
+	private final String codepoint;
+	
+	public Codepoint(String codepoint) {
+		
+		this.codepoint = codepoint;
+	}
+	
+	public String toUTF31() {
+		
+		return "";
+	}
+	
+	public String toUTF16() {
+		
+		return "";
+	}
+	
+	public String toUTF8() {
+		
+		return "";
+	}
+	
+}
