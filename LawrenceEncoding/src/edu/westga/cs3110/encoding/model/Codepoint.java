963db0d397f7702454040a4cd6bb4f7842eeac7f
Adrien Lawrence <firestar9jr@gmail.com>
2026-09-11 17:55:50 -0400

    Constructor Validation Implemented
    Implemented input validation to ensure proper unicode codepoints are
    used with the constructor. Fixed small typo in a method skeleton

--- LawrenceEncoding/src/edu/westga/cs3110/encoding/model/Codepoint.java
+++ LawrenceEncoding/src/edu/westga/cs3110/encoding/model/Codepoint.java
@@ -6,10 +6,29 @@ public class Codepoint {
 	
 	public Codepoint(String codepoint) {
 		
+		if (codepoint == null || codepoint.isBlank()) {
+			throw new IllegalArgumentException("Codepoint cannot be blank");
+		}
+		
+		int value;
+		
+		try {
+			
+			value = Integer.parseUnsignedInt(codepoint, 16);
+			
+		} catch (NumberFormatException e) {
+			
+			throw new IllegalArgumentException("Codepoint must be hexadecimal");
+			
+		}
+		
+		if (value > 0x10FFFF) {
+			throw new IllegalArgumentException("Codepoint is outside the Unicode range");
+		}
 		this.codepoint = codepoint;
 	}
 	
-	public String toUTF31() {
+	public String toUTF32() {
 		
 		return "";
 	}
