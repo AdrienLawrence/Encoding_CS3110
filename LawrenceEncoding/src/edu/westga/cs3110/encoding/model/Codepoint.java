73062e3f1fc6bae8958377a5197ee7dd39090093
Adrien Lawrence <firestar9jr@gmail.com>
2026-09-12 19:45:00 -0400

    ToUTF16 Implemented
    Implemented UTF16 encoder method as well as fixed a type issue in the
    UTF32 encoder that passed a string into a format method expecting an int

--- LawrenceEncoding/src/edu/westga/cs3110/encoding/model/Codepoint.java
+++ LawrenceEncoding/src/edu/westga/cs3110/encoding/model/Codepoint.java
@@ -4,6 +4,8 @@ public class Codepoint {
 	
 	private final String codepoint;
 	
+	private static final int UTF16_OFFSET = 0x10000;
+	
 	public Codepoint(String codepoint) {
 		
 		if (codepoint == null || codepoint.isBlank()) {
@@ -30,12 +32,30 @@ public class Codepoint {
 	
 	public String toUTF32() {
 		
-		return String.format("%08X", this.codepoint);
+		int value = Integer.parseUnsignedInt(this.codepoint, 16);
+		
+		return String.format("%08X", value);
 	}
 	
 	public String toUTF16() {
 		
-		return "";
+		int value = Integer.parseUnsignedInt(this.codepoint, 16);
+		
+		if (value <= 0xFFFF) {
+			
+			return String.format("%04X", value);
+					
+		}
+		
+		value -= UTF16_OFFSET;
+		
+		int leftBits = 0xD800 + (value >> 10);
+		
+		int rightBits = 0xDC00 + (value & 0x3FF);
+		
+		
+		
+		return String.format("%04X%04X", leftBits, rightBits);
 	}
 	
 	public String toUTF8() {
